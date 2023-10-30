881d26c5a32e876538a9063860fb234ffa8e962a
Badr_Annabi <badrannabi@gmail.com>
2023-10-30 20:39:36 +0100

    TASK 11, Sys module

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
-sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stdout.write(str)
 sys.exit(1)
-print_to_stderr("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+print_to_stderr(str)
