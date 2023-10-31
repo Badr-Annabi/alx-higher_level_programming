dd382b53c7da47eac0e782ecb49af74c1a2216da
Badr_Annabi <badrannabi@gmail.com>
2023-10-31 19:31:04 +0100

    TASK5 printing numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 10:
-        print("0{}, ".format(i), end=(''))
-        continue
-    print("{}, ".format(i), end=(''))
-    if i == 99:
-        print("{}".format(i))
+for i in range(100):
+    if i < 99:
+        print("{:02}, ".format(i), end=(''))
+    else:
+        print("{:02}".format(i))
