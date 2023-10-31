98410596057737c4e7c60d3dce44c1c9164e3414
Badr_Annabi <badrannabi@gmail.com>
2023-10-31 19:56:28 +0100

    TASK 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if i == j or j < i:
+            continue
+        elif i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end="")
