521ed27662febcb6d43d56fd2895cb7a82fcd3e3
Badr_Annabi <badrannabi@gmail.com>
2023-10-31 20:36:25 +0100

    TASK 8 SWITCHING from lowercase to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if 'a' <= i <= 'z':
+            up = chr(ord(i) - ord('a') + ord('A'))
+        else:
+            up = i
+        print(up, end="")
+    print()
