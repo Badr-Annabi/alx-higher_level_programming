0407ec4ed997c480e068aabcfd1d797957508c7d
Badr_Annabi <badrannabi@gmail.com>
2023-11-02 22:36:56 +0100

    TASK 6 operators in args

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif op == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif op == "-":
+    elif op == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif op == "-":
+    elif op == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
