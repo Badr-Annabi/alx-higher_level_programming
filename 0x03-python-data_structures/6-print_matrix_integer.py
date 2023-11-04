bd04309d075c3b667dc589d2f07c7a03552ba478
Badr_Annabi <badrannabi@gmail.com>
2023-11-04 21:33:41 +0100

    TASK 6 printing matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,5 @@ def print_matrix_integer(matrix=[[]]):
         if len(line) == 0:
             print()
         for i in range(len(line)):
-            print("{}".format(line[i]),
-                    end="\n" if i is len(line) - 1 else " ")
+            seperator = "\n" if i is len(line) - 1 else " "
+            print("{}".format(line[i]), end=seperator)
