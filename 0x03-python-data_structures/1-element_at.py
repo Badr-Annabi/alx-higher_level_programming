08dff636d06b836eeae8941abc32680eb67663c5
Badr_Annabi <badrannabi@gmail.com>
2023-11-04 17:01:17 +0100

    TASK 1 remaking

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/bin/usr/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return (None)
-    elif idx > len(my_list):
-        return (None)
+        return None
+    elif idx >= len(my_list):
+        return None
     else:
-        return (my_list[idx])
+        return my_list[idx]
