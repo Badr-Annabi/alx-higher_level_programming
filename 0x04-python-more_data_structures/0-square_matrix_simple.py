5ccea245c62ec9cf2ea709d6040c4b0505d728f9
Badr_Annabi <badrannabi@gmail.com>
2023-11-08 18:53:10 +0100

    TASK 0 square value

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda x: list(map(lambda elem: elem**2, x)), matrix))
+    new_matrix = list(map(
+        lambda x: list(map(lambda elem: elem**2, x)), matrix))
     return new_matrix
