20584a641e6c026100b9f9129e7cb67d8d06afa6
Badr_Annabi <badrannabi@gmail.com>
2023-11-08 21:01:09 +0100

    TASK 9 mutiply values with 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dictionary = {}
     for key, value in a_dictionary.items():
-        a_dictionary[key] = value * 2
-    return a_dictionary
+        new_dictionary[key] = value * 2
+    return new_dictionary
