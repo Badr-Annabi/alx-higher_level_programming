2e86ecc7788f80f194c2bc4519bb85f87734a159
Badr_Annabi <badrannabi@gmail.com>
2023-11-09 01:01:08 +0100

    TASK 13 calculating average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    
+    w_values = list(map(lambda x: x[0] * x[1], my_list))
+    total_weight = sum(map(lambda x: x[1], my_list))
+
+    if total_weight == 0:
+        return 0
+    average = sum(w_values) / total_weight
+
+    return average
