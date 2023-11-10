f10f1a643862ffde9b90cbafbc2878f769dd4348
Badr_Annabi <badrannabi@gmail.com>
2023-11-10 17:06:43 +0100

    TASK 16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -9,30 +9,21 @@
  */
 void print_python_list(PyObject *p)
 {
-	int allocated, size, i;
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *list = (PyListObject *)p;
 	const char *type;
 
-	if (strcmp(type, "bytes") == 0)
-	{
-		printf("[*] Python list info\n");
-		printf("[ERROR] Invalid List Object\n");
-		return;
-	}
-
-	PyVarObject *var = (PyVarObject *)p
-
-	size = var->ob_size;
-	allocated = (PyListObject *)p->allocated
-
 	printf("*] Python list info\n");
-	printf("[*] Size of the Python List = %ld", size);
-	printf("[*] Allocated = %ld", allocated);
+	printf("[*] Size of the Python List = %i\n", size);
+	printf("[*] Allocated = %i\n", list->allocated);
 
 	for (i = 0; i < size; i++)
 	{
-		type = list->ob_item[i]->ob_type->tp_name;
-		item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, type);
+		type = (list->ob_item[i])->ob_type->tp_name;
+		printf("Element %i: %s\n", i, type);
+		if (!strcmp(type, "bytes"))
+			print_python_bytes(list->ob_item[i]);
 	}
 }
 
@@ -42,31 +33,28 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-	unsigned char i, size;
-	PyBytesObject *bytes = (PyBytesObject *)p;
+	long int size;
+	int i;
+	char *str = NULL;
 
 	printf("[.] bytes object info\n");
-	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	if (!PyBytes_check(p))
 	{
-		printf("[ERROR] Invalid Bytes Object\n");
+		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	printf("size: %ld\n", ((PyVarObject *)p)->ob_size);
-	printf("trying string: %s\n", butes->ob_sval);
+	PyBytes_AaStringAndSize(p, &str, &size);
 
-	if (((PyVarObject *)p)->ob_size > 10)
-		size = 10;
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", str);
+
+	if (size < 10)
+		print("  first %li bytes:", size + 1);
 	else
-		size = ((PyVarObject *)p)->ob_size + 1;
-	printf("first %d bytes: ", size);
-	for (i = 0; i < size; i++)
-	{
-		printf("%02hhx", bytes->ob_sval[i]);
-		if (i == (size - 1))
-			printf("\n");
-		else
-			printf(" ");
-	}
+		printf("  first 10 bytes:");
 
+	for (i = 0; i < size && i < 10; i++)
+		printf(" %02hhx", str[i]);
+	printf("\n");
 }
