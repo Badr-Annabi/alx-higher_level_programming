c4658763e2a4b4f749cc2c3b55dcb719e4dbe6be
Badr_Annabi <badrannabi@gmail.com>
2023-11-20 20:02:33 +0100

    TASK 9 Bytecode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,9 +5,9 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception("Too far")
-            result += (a ** b) / i
+            result += a ** b / i
         except Exception:
-            result += b + a
+            result = b + a
             break
     return result
 
