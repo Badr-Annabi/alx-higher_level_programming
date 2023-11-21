dfd04375c311da2bf998dcc15400757a1d0453e8
Badr_Annabi <badrannabi@gmail.com>
2023-11-21 21:31:21 +0100

    TASK2 adding exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Module Name: square_module
+
+This module defines a simple Square class.
+
+Class:
+    Square: Defines a square.
+
+Atrribute:
+    None
+Usage:
+    new_square = Square(n)
+
+"""
+
+
+class Square:
+    """
+    Initializes a new instance of square class.
+
+
+    Args:
+        __size (int): The size of the square.
+
+        Return:
+            None
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of square class.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
