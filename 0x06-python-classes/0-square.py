e1204f2875d565a5d25b44a147ebe5000f1bc213
Badr_Annabi <badrannabi@gmail.com>
2023-11-21 20:55:38 +0100

    TASK0 empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module Name: square_module
+
+This module defines a simple Square class.
+
+Class:
+    Square: Defines a square.
+    
+Atrribute:
+    None
+Usage:
+    new_square = Square()
+
+"""
+class Square:
+    """
+    a class that defines a square.
+
+    Attributes:
+    (None)
+
+    Methods:
+    (None)
+    """
+    pass
