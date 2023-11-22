36679c3037e6a6d30380193bf99a6eb84586301f
Badr_Annabi <badrannabi@gmail.com>
2023-11-22 22:34:06 +0100

    TASK 7 Singly linked list and Node classes

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -41,23 +41,70 @@ class Node:
 
     @property
     def data(self):
+        """
+            Returning the node's data
+
+            Return:
+                the node's data
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+            Sets the data
+
+            Args:
+                None
+
+            Return:
+                None
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """
+            Returning the next node
+
+            Return:
+                The next node
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+            Sets the next node
+
+            Args:
+                None
+
+            Return:
+                None
+        """
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
+
+"""
+Module Name: Singly linked list module
+
+This module defines a node of a singly
+linked list class.
+
+Classes:
+    Node: Defines a node of a singly linked list.
+    SinglyLinkedList: Defines a SLL
+
+Atrribute:
+    None
+Usage:
+    Creating Nodes.
+"""
 
 
 class SinglyLinkedList:
@@ -72,6 +119,13 @@ class SinglyLinkedList:
         self.__head = None
 
     def sorted_insert(self, value):
+        """
+        Inserts the new node and sorts the list.
+
+        Args: value(the value of the new node)
+
+        Return: None
+        """
         new_node = Node(value)
         if self.__head is None or self.__head.data >= value:
             new_node.next_node = self.__head
@@ -84,6 +138,13 @@ class SinglyLinkedList:
             tmp.next_node = new_node
 
     def __str__(self):
+        """
+        Defining our own print: prints all the list
+
+        Args: None
+
+        Return: All the nodes.
+        """
         tmp = self.__head
         printed = ""
         while tmp.next_node is not None:
