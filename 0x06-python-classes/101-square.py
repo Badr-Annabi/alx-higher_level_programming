153ced2fde2d73d0a8819da5ad97050bc09ae8d3
Badr_Annabi <badrannabi@gmail.com>
2023-11-22 02:58:55 +0100

    TASK 8

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -83,9 +83,9 @@ class Square:
             Return:
                 None
         """
-        if type(value) is not tuple or len(value) != 2\
-           or type(value[0]) is not int or type(value[1])\
-           is not int or value[0] < 0 or value[1] < 0:
+        if not isinstance(value, tuple) or len(value) != 2\
+           or not isinstance(value[0], int) or not isinstance(
+               value[1], int) or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
