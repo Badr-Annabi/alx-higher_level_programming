1d09c4c91c19c2b09022baa7b7bcf9d7bd1b43e3
Badr_Annabi <badrannabi@gmail.com>
2023-11-27 20:22:28 +0100

    TASK0 creating empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
+"""
+Module Name: square_module
+
+This module defines a simple Square class.
+
+Class:
+    Square: Defines a square.
+
+Atrribute:
+    None
+Usage:
+    new_square = Square()
+"""
 
 
 class Rectangle:
+    """
+    a class that defines a rectangle.
+
+    Attributes:
+    (None)
+
+    Methods:
+    (None)
+    """
     pass
