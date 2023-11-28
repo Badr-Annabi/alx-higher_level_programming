e3464ebd681eee9c3111958c064763fb735c6207
Badr_Annabi <badrannabi@gmail.com>
2023-11-28 19:58:41 +0100

    Fixing bugs

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,35 +1,7 @@
 #!/usr/bin/python3
-
-"""
-Module Name: LockedClass.
-
-This module (Class) prevents the user
-from dynamically creating new
-instance attributes, except if the
-new instance attribute is called
-first_name..
-
-Class:
-    LockedClass.
-
-Atrribute:
-    __slots__
-Usage:
-    LockedClass()
-"""
+"""LockedClass module."""
 
 
 class LockedClass:
-
-    """
-    A class that defines a rectangle
-
-    Attribute:
-    __slots__: defines a tuple of
-    allowed attribute names
-
-    Methods:
-    None
-    """
-
+    """LockeClass claa containing only __slots__."""
     __slots__ = ['first_name']
