8abae599d45f28502bd54368d77958f24b86ec03
Badr_Annabi <badrannabi@gmail.com>
2023-12-02 03:35:09 +0100

    all

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
+"""Defines an integer addition function."""
+
 
 def add_integer(a, b=98):
+    """
+     Adds two numbers and returns their sum.
+
+    Parameters:
+        a (int/float): The first number.
+        b (int/float): The second number. Default to 98.
+
+    Raises:
+        TypeError
+
+    Returns:
+        int: The sum of a and b
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
-    # a = int(a)
-    # b = int(b)
     return int(a) + int(b)
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,6 +4,8 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     i = 0
+    while i < len(text) and text[i] == " ":
+        i += 1
     while i < len(text):
         if text[i] in ".?:":
             print(text[i], end='')
