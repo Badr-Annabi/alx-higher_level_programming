d92a16729f18cdc664ffaa23f5278a87c3581ed0
Badr_Annabi <badrannabi@gmail.com>
2023-12-02 03:38:59 +0100

    all2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
+"""Defines a matrix division function."""
+
 
 def matrix_divided(matrix, div):
+    """
+    Divide all elements of a matrix.
+
+    Args:
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
+
+    Raises:
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
+        ZeroDivisionError: If div equals to 0.
+
+    Returns:
+        A new matrix representing the result of the division.
+    """
+
     if not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix "
                         "(list of lists)of integers/floats")
