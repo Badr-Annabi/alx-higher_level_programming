2eabe957ec8b9f2033c3cd9394cc08465d23b4cc
Badr_Annabi <badrannabi@gmail.com>
2023-12-04 21:33:39 +0100

    add documentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,4 +7,9 @@ is an instance of a specific class.
 
 
 def is_same_class(obj, a_class):
+    """
+    Returns True if an object
+    is an instance of a specific class, and
+    False otherwise
+    """
     return type(obj) is a_class
