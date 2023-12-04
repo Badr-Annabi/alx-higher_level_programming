78b8fd43ce439eede8173268608f5474bb200840
Badr_Annabi <badrannabi@gmail.com>
2023-12-04 23:53:58 +0100

    TASK 9 fix bugs

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -14,9 +14,9 @@ class Rectangle(BaseGeometry):
         self.__width = width
         super().integer_validator("height", height)
         self.__height = height
-    
+
     def area(self):
         return self.__width * self.__height
-    
+
     def __str__(self):
         return ("[Rectangle] {}/{}".format(self.__width, self.__height))
