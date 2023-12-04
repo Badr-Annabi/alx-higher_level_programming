d84cb6fc163b43e2cbf6ac8ebdb22a7bc5709b18
Badr_Annabi <badrannabi@gmail.com>
2023-12-05 00:43:30 +0100

    TASK10 square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Module that Defines a Square Class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """"""
+
+    def __init__(self, size):
+        """
+
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area"""
+        return (self.__size)**2
+
+    def __str__(self):
+        """Prints the Rectangle"""
+        return ("[Rectangle]".format(self.__size))
