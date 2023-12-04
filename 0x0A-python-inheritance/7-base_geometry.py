b82cd6781ff5a3c1a0056e92306a98e2a1043220
Badr_Annabi <badrannabi@gmail.com>
2023-12-04 22:37:16 +0100

    TASK 7  Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define an empty class"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
