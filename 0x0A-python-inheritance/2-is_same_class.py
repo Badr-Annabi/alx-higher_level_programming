f7d17e8680d146864343df11074e7c785f435a4f
Badr_Annabi <badrannabi@gmail.com>
2023-12-04 21:31:09 +0100

    TASK2 check for exact instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+Define a function that checks if an object
+is an instance of a specific class.
+"""
+
+
+def is_same_class(obj, a_class):
+    return type(obj) is a_class
