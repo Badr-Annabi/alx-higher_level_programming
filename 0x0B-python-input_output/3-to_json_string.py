4a126825509ef5aeecbfdd64485c18f78970b42f
Badr_Annabi <badrannabi@gmail.com>
2023-12-06 00:11:58 +0100

    TASK 3 rep of json (json.dumps)~

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -9,4 +9,9 @@ of an object (string):
 
 
 def to_json_string(my_obj):
+    """
+    Define  a function
+    that returns the JSON representation
+    of an object (string):
+    """
     return json.dumps(my_obj)
