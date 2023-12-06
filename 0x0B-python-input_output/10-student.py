dc2d2f12c744ede3bbc9b05df650c877734fb0f2
Badr_Annabi <badrannabi@gmail.com>
2023-12-06 02:52:00 +0100

    TASK 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Module that defines a student"""
+
+
+class Student:
+    """
+    Class that defines a student by:
+
+    Attributes: first_name, last_name, age.
+
+    Methods: __init__. to_json.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        if attrs:
+            fill_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    fill_dict[attr] = getattr(self, attr)
+            return fill_dict
+        return self.__dict__
