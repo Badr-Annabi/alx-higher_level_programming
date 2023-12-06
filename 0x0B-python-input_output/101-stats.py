1adc4b18c877ec83bf66178173a75dd1de1bd5cc
Badr_Annabi <badrannabi@gmail.com>
2023-12-06 04:21:41 +0100

    TASK 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,8 +7,8 @@ this script reads the stdin line by line and computes metrices
 import sys
 
 
-size = 0
-status = {"200": 0, "301": 0, "400": 0, "401": 0,
+total_size = 0
+status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
           "403": 0, "404": 0, "405": 0, "500": 0}
 i = 0
 try:
@@ -16,28 +16,28 @@ try:
         slice = line.split()
         if len(slice) >= 2:
             a = i
-            if slice[-2] in status:
-                status[slice[-2]] += 1
+            if slice[-2] in status_code:
+                status_code[slice[-2]] += 1
                 i += 1
             try:
-                size += int(slice[-1])
+                total_size += int(slice[-1])
                 if a == i:
                     i += 1
             except FileNotFoundError:
                 if a == i:
                     continue
         if i % 10 == 0:
-            print("File size: {:d}".format(size))
-            for key, value in sorted(status.items()):
+            print("File size: {:d}".format(total_size))
+            for key, value in sorted(status_code.items()):
                 if value:
                     print("{:s}: {:d}".format(key, value))
-    print("File size: {:d}".format(size))
-    for key, value in sorted(status.items()):
+    print("File size: {:d}".format(total_size))
+    for key, value in sorted(status_code.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
 
 except KeyboardInterrupt:
-    print("File size: {:d}".format(size))
-    for key, value in sorted(status.items()):
+    print("File size: {:d}".format(total_size))
+    for key, value in sorted(status_code.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
