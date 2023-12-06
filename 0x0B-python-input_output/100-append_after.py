c3f179812b51536a2ea5f2a6a0cd0909f53013b0
Badr_Annabi <badrannabi@gmail.com>
2023-12-06 03:01:28 +0100

    TASK 13 insert new line

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Define  a function
+that  that inserts a line of text to a file, after
+each line containing a specific string”
+"""
+import json
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+     Inserts a line of text to a file,
+     after each line containing a specific string”
+    """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
