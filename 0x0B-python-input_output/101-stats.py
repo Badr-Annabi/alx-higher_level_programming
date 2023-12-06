782497ac670a7e70c8ecafa25fcd63d0bea2c7d5
Badr_Annabi <badrannabi@gmail.com>
2023-12-06 04:17:40 +0100

    TASK 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,40 +1,43 @@
 #!/usr/bin/python3
+"""
+101-stats
 
-"""reads stdin line by line and computes metrics."""
+this script reads the stdin line by line and computes metrices
+"""
 import sys
 
 
-total_size = 0
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
+size = 0
+status = {"200": 0, "301": 0, "400": 0, "401": 0,
+          "403": 0, "404": 0, "405": 0, "500": 0}
 i = 0
 try:
     for line in sys.stdin:
         slice = line.split()
         if len(slice) >= 2:
-            j = i
-            if slice[-2] in status_codes:
-                status_codes[slice[-2]] += 1
+            a = i
+            if slice[-2] in status:
+                status[slice[-2]] += 1
                 i += 1
             try:
-                total_size += int(slice[-1])
-                if j == i:
+                size += int(slice[-1])
+                if a == i:
                     i += 1
             except FileNotFoundError:
-                if j == i:
+                if a == i:
                     continue
         if i % 10 == 0:
-            print("File size: {:d}".format(total_size))
-            for key, value in sorted(status_codes.items()):
+            print("File size: {:d}".format(size))
+            for key, value in sorted(status.items()):
                 if value:
                     print("{:s}: {:d}".format(key, value))
-    print("File size: {:d}".format(total_size))
-    for key, value in sorted(status_codes.items()):
+    print("File size: {:d}".format(size))
+    for key, value in sorted(status.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
 
 except KeyboardInterrupt:
-    print("File size: {:d}".format(total_size))
-    for key, value in sorted(status_codes.items()):
+    print("File size: {:d}".format(size))
+    for key, value in sorted(status.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
