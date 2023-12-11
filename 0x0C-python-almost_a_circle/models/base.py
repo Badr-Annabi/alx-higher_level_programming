38c3065b1980f7d909a9302d1602bf5b7ff9389e
Badr_Annabi <badrannabi@gmail.com>
2023-12-11 19:27:47 +0100

    change from_json_string methid

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,7 +43,7 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        if json_string is None or json_string == "[]":
+        if json_string is None or json_string == "":
             return []
         else:
             return json.loads(json_string)
