1c662b58393599daf7563915982975911b91725a
Badr_Annabi <badrannabi@gmail.com>
2023-12-11 23:40:29 +0100

    Square

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module for Square unittest"""
+import unittest
+from models.base import Base
+from models.square import Square
+import io
+
+
+class TestSquare(unittest.TestCase):
+    """Test Base Class"""
+
+    def setUp(self):
+        Base.__nb_objects = 0
+        pass
+
+    '--------------- Test #2 ------------------'
+
+    def test_class(self):
+        """Test for Square Class"""
+        self.assertEqual(str(
+            Square), "<class 'models.square.Square'>")
+    
+    def test_inheritance(self):
+        """Test inheritance Square->Base"""
+        self.assertTrue(isinstance(Square, Base))
+    
