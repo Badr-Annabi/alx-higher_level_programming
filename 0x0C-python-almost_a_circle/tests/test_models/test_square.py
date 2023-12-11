b8bce4159cbe52d9ac5a5631e0886557718b809d
Badr_Annabi <badrannabi@gmail.com>
2023-12-12 00:41:43 +0100

    add constructor square

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,28 +1,30 @@
-# #!/usr/bin/python3
-# """Module for Square unittest"""
-# import unittest
-# from models.base import Base
-# from models.square import Square
-# import io
+#!/usr/bin/python3
+"""Module for Square unittest"""
+import unittest
+from models.base import Base
+from models.square import Square
+import io
 
 
-# class TestSquare(unittest.TestCase):
-#     """Test Base Class"""
+class TestSquare(unittest.TestCase):
+    """Test Base Class"""
 
-#     def setUp(self):
-#         Base.__nb_objects = 0
-#         pass
+    def setUp(self):
+        Base.__nb_objects = 0
+        pass
 
-#     '--------------- Test #2 ------------------'
+    '--------------- Test #2 ------------------'
 
-#     def test_class(self):
-#         """Test for Square Class"""
-#         self.assertEqual(str(
-#             Square), "<class 'models.square.Square'>")
+    def test_class(self):
+        """Test for Square Class"""
+        self.assertEqual(str(
+            Square), "<class 'models.square.Square'>")
     
-#     def test_inheritance(self):
-#         """Test inheritance Square->Base"""
-#         self.assertTrue(issubclass(Square, Base))
+    def test_inheritance(self):
+        """Test inheritance Square->Base"""
+        self.assertTrue(issubclass(Square, Base))
     
-#     def test_constructor(self):
-        
+    def test_constructor(self):
+        """Test constructor"""
+        with self.assertRaises(TypeError) as e:
+            r = Square()
