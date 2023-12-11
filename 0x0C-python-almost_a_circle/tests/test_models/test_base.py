9100ac2c7199bc103259e19fe2f3d06c9179a360
Badr_Annabi <badrannabi@gmail.com>
2023-12-11 22:17:37 +0100

    add test load

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -168,6 +168,30 @@ were given"
         self.assertEqual(str(r1), str(r2))
         self.assertFalse(r1 is r2)
         self.assertFalse(r1 == r2)
+    
+    '======== Test for 19 =========='
+
+    def test_load_from_file(self):
+        ''' Test load_from_file'''
+        r1 = Rectangle(4, 8, 16, 40)
+        r2 = Rectangle(85, 98)
+        list_in = [r1, r2]
+        Rectangle.save_to_file(list_in)
+        list_out = Rectangle.load_from_file()
+        self.assertNotEqual(id(list_in[0]), id(list_out[0]))
+        self.assertEqual(str(list_in[0]), str(list_out[0]))
+        self.assertNotEqual(id(list_in[1]), id(list_out[1]))
+        self.assertEqual(str(list_in[1]), str(list_out[1]))
+
+        s1 = Square(55)
+        s2 = Square(78, 89, 41)
+        list_in = [s1, s2]
+        Square.save_to_file(list_in)
+        list_out = Square.load_from_file()
+        self.assertNotEqual(id(list_in[0]), id(list_out[0]))
+        self.assertEqual(str(list_in[0]), str(list_out[0]))
+        self.assertNotEqual(id(list_in[1]), id(list_out[1]))
+        self.assertEqual(str(list_in[1]), str(list_out[1]))
         
         # r2 = Rectangle(2, 4)
         # Rectangle.save_to_file([r2])
