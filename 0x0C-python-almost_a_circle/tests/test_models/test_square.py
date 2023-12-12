fcf93a436da5544761b2dca1b37a1be0f446fea6
Badr_Annabi <badrannabi@gmail.com>
2023-12-12 01:01:39 +0100

    add instantiation

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -37,3 +37,42 @@ class TestSquare(unittest.TestCase):
 were given"
 
         self.assertEqual(str(e.exception), s)
+    
+    def test_raises(self):
+
+        """ Test cases"""
+        s = Square(10)
+        self.assertEqual(str(type(s)), "<class 'models.square.Square'>")
+        self.assertTrue(isinstance(s, Base))
+        with self.assertRaises(TypeError) as context:
+            s = Square("string")
+        self.assertEqual(str(context.exception), "width must be an integer")
+
+        with self.assertRaises(TypeError) as context:
+            s = Square(5, 5, "string")
+        self.assertEqual(str(context.exception), "x must be an integer")
+
+        with self.assertRaises(TypeError) as context:
+            s = Square(5, 5, 5, "string")
+        self.assertEqual(str(context.exception), "y must be an integer")
+
+        """ ValueError Exception """
+        with self.assertRaises(ValueError) as context:
+            s = Square(-5, 5)
+        self.assertEqual(str(context.exception), "width must be > 0")
+
+        with self.assertRaises(ValueError) as context:
+            s = Square(0, 5)
+        self.assertEqual(str(context.exception), "width must be > 0")
+
+        with self.assertRaises(ValueError) as context:
+            s = Square(5, 5, -5)
+        self.assertEqual(str(context.exception), "x must be >= 0")
+
+        with self.assertRaises(ValueError) as context:
+            s = Square(5, 5, 5, -5)
+        self.assertEqual(str(context.exception), "y must be >= 0")
+
+
+if __name__ == '__main__':
+    unittest.main()
