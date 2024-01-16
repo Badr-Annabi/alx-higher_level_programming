1d897c99dbc7b8feaf5c6da37dfc1bce83959479
Badr_Annabi <badrannabi@gmail.com>
2024-01-17 00:07:21 +0100

    TASK 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
-SELECT score, name from second_table WHERE name IS NOT NULL AND name != ''
+SELECT score, name FROM second_table WHERE name IS NOT NULL
 ORDER BY score DESC;
