25a70bc3b351e2cdba975a10d9c2c059e2f10a7e
Badr_Annabi <badrannabi@gmail.com>
2024-01-17 23:10:57 +0100

    9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- that lists all cities contained in the database hbtn_0d_usa.
+SELECT DISTINCT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id;
+ORDER BY cities.id
