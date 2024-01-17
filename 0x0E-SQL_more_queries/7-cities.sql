89e0874590e0bd22b10b2de383db3b1b30b1e3c9
Badr_Annabi <badrannabi@gmail.com>
2024-01-17 21:34:27 +0100

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL;
-	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.state(id)
+	FOREIGN KEY (state_id) REFERENCES state(id)
 );
