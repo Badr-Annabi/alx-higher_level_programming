34b54402a364c979d886086740b3e083b7b3011c
Badr_Annabi <badrannabi@gmail.com>
2024-01-17 21:37:01 +0100

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- creates the database hbtn_0d_usa and the table states (in the database)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL;
 	FOREIGN KEY (state_id) REFERENCES states(id)
