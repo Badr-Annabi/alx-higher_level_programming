8c595594294244b4b649eb3990f15ab533b59834
Badr_Annabi <badrannabi@gmail.com>
2024-01-18 02:06:22 +0100

    19 (102)

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,9 +1,7 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
-FROM tv_genres
-INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
+SELECT title, SUM(rate) AS rating
+FROM tv_shows
 INNER JOIN tv_show_ratings
-ON tv_show_genres.show_id = tv_show_ratings.show_id
-GROUP BY tv_genres.id
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY title
 ORDER BY rating DESC;
