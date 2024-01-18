20c451c3ff3263e7cdf1e0bf4a896c9bb6c4f51e
Badr_Annabi <badrannabi@gmail.com>
2024-01-18 02:12:08 +0100

    20 (103)

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT name, SUM(rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show_ratings
+ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY name
+ORDER BY rating DESC;
