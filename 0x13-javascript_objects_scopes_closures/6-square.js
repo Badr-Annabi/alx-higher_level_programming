b3775aba1349f71c0cf042eadf643a88be39d80f
Badr Annabi <sofiaannabi@sofias-MacBook-Pro.local>
2024-02-13 23:34:12 +0100

    TASK 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const SquareParent = require('./5-square');
+
+class Square extends SquareParent {
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
