8994f797617a5b8f73db005d5f1beae86668bf94
Badr Annabi <sofiaannabi@sofias-MacBook-Pro.local>
2024-02-13 01:44:13 +0100

    TASK 17

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12,
+  incr: function () {
+    this.value++;
+    return this.value;
+  }
+};
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
