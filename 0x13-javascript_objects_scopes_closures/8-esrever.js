ffcbb853d685417b2608842e81b77c19010c34da
Badr Annabi <sofiaannabi@sofias-MacBook-Pro.local>
2024-02-14 03:38:44 +0100

    TASK 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  list.reverse();
+  const len = list.length;
+  let tmp;
+  for (let i = 0; i < len / 2; i++) {
+    tmp = list[i];
+    list[i] = list[len - i - 1];
+    list[len - i - 1] = tmp;
+  }
   return list;
 };
