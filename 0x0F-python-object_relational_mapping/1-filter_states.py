335a3048a643894ebb18bbda5305a83fd449854d
Badr-annabi <badrannabi@gmail.com>
2024-02-16 18:25:01 +0000

    TASK 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""shows all content of a table in a database with letter N"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8")
+    cur = conn.cursor()
+    cur.execute(
+            "SELECT * FROM states \
+            WHERE name LIKE 'N%' \
+            ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
