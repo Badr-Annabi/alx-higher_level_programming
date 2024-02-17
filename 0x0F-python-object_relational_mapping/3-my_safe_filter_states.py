47c43313ad92f7bfe4434b3ec3e37778caf673c3
Badr-annabi <badrannabi@gmail.com>
2024-02-17 14:59:19 +0000

    TASK 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""deletes whole table"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    search_name = sys.argv[4]
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8")
+    cur = conn.cursor()
+    query = ("SELECT * FROM states \
+            WHERE BINARY name = %s  \
+            ORDER BY id ASC")
+    cur.execute(query, search_name)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
