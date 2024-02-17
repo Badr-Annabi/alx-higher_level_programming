62db755b67d48c035a265871e143cd6e79bf2cd8
Badr-annabi <badrannabi@gmail.com>
2024-02-17 22:45:18 +0000

    TASK 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,18 +2,8 @@
 """creates first class using SQLALchemy"""
 
 
-from sqlalchemy import create_engine, Column, String, Integer
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine(
-        'mysql+mysqldb://{usr}:{pss}@ \
-                localhost:3306/ \
-                {hbtn_0e_6_usa}'.format("root", "root", "hbtn_0e_6_usa \
-                "), pool_pre_ping=True)
-
-Session = sessionmaker(bind=engine)
-session = Session()
 
 Base = declarative_base()
 
