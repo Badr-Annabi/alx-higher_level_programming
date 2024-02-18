4de89cdb53f0ceba675a0fd17c1b3468ffd238e3
Badr-annabi <badrannabi@gmail.com>
2024-02-18 16:12:37 +0000

    TASK 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""List first element of the table States"""
+"""List first State object from the database hbtn_0e_6_usa"""
 
 
 import sys
 from model_state import Base, State
-
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
@@ -15,10 +14,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
+    states = session.query(State).order_by(State.id).first()
 
     if (states):
-        state = states[0]
-        print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(states.id, states.name))
     else:
         print("Nothing")
