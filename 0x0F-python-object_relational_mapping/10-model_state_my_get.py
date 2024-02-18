a6dc58cae97e964be8ae0ea5a5b8622f89180a5a
Badr-annabi <badrannabi@gmail.com>
2024-02-18 16:43:21 +0000

    TASK 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""List first State object from the database hbtn_0e_6_usa"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    name_search = sys.argv[4]
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(
+            State).filter(
+                    State.name == name_search).first()
+
+    if (state):
+        print("{}".format(state.id))
+    else:
+        print("Not found")
