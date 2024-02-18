795cff2f32e8597300cab7de733d0aa0a8f919e8
Badr-annabi <badrannabi@gmail.com>
2024-02-18 20:15:56 +0000

    TASK 14 (join)

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""List all elements of the table States"""
+
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_city = session.query(
+            State.name, City.id, City.name).join(
+                    City, City.state_id == State.id).order_by(City.id)
+    all_states = state_city.all()
+    for state, city_id, city in all_states:
+        print("{}: ({}) {}".format(state, city_id, city))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""creates City class using SQLALchemy"""
+
+
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    creates a city table
+    __tablename__: cities
+    Attributes:
+    id, name, state_id
+    """
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('States.id'), nullable=False)
