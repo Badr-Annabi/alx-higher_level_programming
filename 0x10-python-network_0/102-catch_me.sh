aa0cad7136ebc2835cf189c8136ff340c63ec172
Badr-annabi <badrannabi@gmail.com>
2024-02-29 23:50:17 +0000

    TASK 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#This Bash script sends a req to 0.0.0.0:5000/catch_me
-curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin" "Origin: HolbertonSchool"
+#This Bash script sends a req to 0.0.0.0:5000/catch_me so it causes to respond a message "You got me!"
+curl -sL -X PUT -H "Origin" "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
