d0fbf5acb9bb525a302e9fedef3c765a2c66175a
Badr-annabi <badrannabi@gmail.com>
2024-02-29 22:25:56 +0000

    TASK 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#!This Bash script takes in a URL, sends a req to that URL and displays
-#the size of the body of the res
-
+#!This Bash script takes in a URL, sends a req to that URL
 curl -sI "$1" | awk '/Content-Length/ {print $2}'
