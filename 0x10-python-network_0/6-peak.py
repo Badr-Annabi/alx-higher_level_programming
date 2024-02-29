0b6a4a396b989fde87197b6c195600e928af6b58
Badr-annabi <badrannabi@gmail.com>
2024-02-29 23:59:39 +0000

    TASK 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+This function finds a peak in a list of unsorted integers.
+"""
+
+def find_peak(list_of_integers):
+    """ This function finds the peak """
+    if not list_of_integers:
+        return None
+    list_of_integers.sort()
+    return list_of_integers[-1]
