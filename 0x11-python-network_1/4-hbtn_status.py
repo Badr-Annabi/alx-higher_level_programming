d394406ad3a4af1fc15d5ff214144528e7379ab1
Badr-annabi <badrannabi@gmail.com>
2024-03-01 21:02:50 +0000

    TASK 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" This Python script fetches https://alx-intranet.hbtn.io/status """
+
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    res = requests.get(url)
+    body = response.text
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
