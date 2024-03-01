43b777b670c529cdaf65cf271de0cbbd3a7078fe
Badr-annabi <badrannabi@gmail.com>
2024-03-01 16:19:30 +0000

    TASK 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,7 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
+    print("Body response:")
     print(f"    - type: {type(html)}")
     print(f"    - content: {html}")
     print(f"    - utf8 content: {html.decode('utf-8')}")
