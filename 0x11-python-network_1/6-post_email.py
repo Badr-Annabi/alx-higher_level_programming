75247091b4d95a34a924f96552fec3e366b785b8
Badr-annabi <badrannabi@gmail.com>
2024-03-01 21:26:25 +0000

    TASK 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+This Python script takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a
+parameter, and finally displays the body of the response.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    res = requests.post(url, data=payload)
+    print(res.text)
