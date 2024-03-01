1b9e6ba0457e206f81424d3976ef795477ff001d
Badr-annabi <badrannabi@gmail.com>
2024-03-01 16:17:27 +0000

    TASK 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" This python script fetches the status from ALX intranet """
+
+
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+    print(f"    - type: {type(html)}")
+    print(f"    - content: {html}")
+    print(f"    - utf8 content: {html.decode('utf-8')}")
