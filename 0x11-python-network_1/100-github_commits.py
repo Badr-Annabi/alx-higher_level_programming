a8f693a109dc92a19a828459e09bca304214511e
Badr-annabi <badrannabi@gmail.com>
2024-03-01 23:13:24 +0000

    TASK 10 backend challenge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+This Python script takes 2 arguments in order to solve this challenge.
+"""
+import ss
+import requests
+
+if __name__ == "__main__":
+    url = "https://developer.github.com/v3/repos/{}/{}/commits/".format(
+            sys.argv[2], sys.argv[1])
+    res = request.get(url)
+    commit = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commit[i].get("sha"),
+                commit[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
