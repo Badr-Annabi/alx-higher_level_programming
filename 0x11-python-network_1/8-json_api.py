270b92d20c1c1d00460b632a737c43bf80c9e5a3
Badr-annabi <badrannabi@gmail.com>
2024-03-01 21:48:22 +0000

    TASK 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+This Python script takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': q}
+    res = requests.get(url, data=payload)
+    try:
+        json_res = res.json()
+        if json_res:
+            print("[{}] {}".format(
+                json_res.get('id'), json_res.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
