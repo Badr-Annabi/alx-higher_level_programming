f9b8abdb3853b4308b2ae167b4ef8cd4d82c3301
Badr-annabi <badrannabi@gmail.com>
2024-03-01 22:27:42 +0000

    TASK 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,11 +8,10 @@ import sys
 
 if __name__ == "__main__":
 
-    res = requests.get(
-            'https://api.github.com/user', auth=(
-                sys.argv[1], sys.argv[2]))
+    res = requests.get('https://api.github.com/user',
+            auth=(sys.argv[1], sys.argv[2]))
     json_res = res.json()
     try:
-        print(json['id'])
+        print(json_res['id'])
     except:
         print("None")
