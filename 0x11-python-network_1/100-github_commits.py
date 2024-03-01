66c9f4bd07d2c1fbf6a44f38659e5e90d5c85805
Badr-annabi <badrannabi@gmail.com>
2024-03-01 23:25:49 +0000

    TASK 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
-
-Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
+This Python script takes 2 arguments in order to solve this challenge.
 """
 import sys
 import requests
@@ -11,8 +10,8 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
-    r = requests.get(url)
-    commits = r.json()
+    res = requests.get(url)
+    commits = res.json()
     try:
         for i in range(10):
             print("{}: {}".format(
