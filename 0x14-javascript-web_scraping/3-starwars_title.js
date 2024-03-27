2d6cf2679ed242c81b024390c6f28ca898b32818
Badr-annabi <badrannabi@gmail.com>
2024-03-27 00:04:01 +0000

    TASK 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = `https://swapi-api.alx-tools.com/api/films`;
+const id = process.argv[2];
+
+request.get(`${url}/${id}`, (error, res, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});
