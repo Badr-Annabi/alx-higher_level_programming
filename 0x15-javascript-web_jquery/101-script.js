84a12866dcbe0e740090baadac7bc42c9771555b
Badr-annabi <badrannabi@gmail.com>
2024-04-04 01:13:17 +0000

    TASK 11

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  $('#add_item').on('click', () => {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+  $('#remove_item').on('click', () => {
+    $('ul.my_list li:last-child').remove();
+  });
+  $('#clear_list').on('click', () => {
+    $('ul.my_list').empty();
+  });
+});
